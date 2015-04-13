c5045eb4c735b8d20a0307684e106618a78de9fb
wopqw <wopqw@yandex.ru>
2015-04-13 11:50:01 +0300

    now working

---
+++ myUpload/src/main/java/com/example/controllers/FileController.java
@@ -0,0 +1,67 @@
+package com.example.controllers;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import com.example.model.FileMeta;
+@Controller
+@RequestMapping("/controller")
+public class FileController {
+
+    LinkedList<FileMeta> files = new LinkedList<FileMeta>();
+    FileMeta fileMeta = null;
+    /***************************************************
+     * URL: /rest/controller/upload
+     * upload(): receives files
+     * @param request : MultipartHttpServletRequest auto passed
+     * @param response : HttpServletResponse auto passed
+     * @return LinkedList<FileMeta> as json format
+     ****************************************************/
+    @RequestMapping(value="/upload", method = RequestMethod.POST)
+    public @ResponseBody LinkedList<FileMeta> upload(MultipartHttpServletRequest request, HttpServletResponse response) {
+
+         Iterator<String> itr =  request.getFileNames();
+         MultipartFile mpf = null;
+         while(itr.hasNext()){
+             mpf = request.getFile(itr.next());
+             System.out.println(mpf.getOriginalFilename() +" uploaded! "+files.size());
+             if(files.size() >= 10)
+                 files.pop();
+             fileMeta = new FileMeta();
+             fileMeta.setFileName(mpf.getOriginalFilename());
+             fileMeta.setFileSize(mpf.getSize()/1024+" Kb");
+             fileMeta.setFileType(mpf.getContentType());
+             try {
+                fileMeta.setBytes(mpf.getBytes());
+                 FileCopyUtils.copy(mpf.getBytes(), new FileOutputStream(mpf.getOriginalFilename()));
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+             files.add(fileMeta);
+         }
+        return files;
+    }
+    @RequestMapping(value = "/get/{value}", method = RequestMethod.GET)
+     public void get(HttpServletResponse response,@PathVariable String value){
+         FileMeta getFile = files.get(Integer.parseInt(value));
+         try {
+                response.setContentType(getFile.getFileType());
+                response.setHeader("Content-disposition", "attachment; filename=\""+getFile.getFileName()+"\"");
+                FileCopyUtils.copy(getFile.getBytes(), response.getOutputStream());
+         }catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+         }
+     }
+}
